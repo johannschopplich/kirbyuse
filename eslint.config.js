aef6b07401f1ae64bb0d9ca5a52f0679ce70dc4d
Johann Schopplich <mail@johannschopplich.com>
2024-06-18 21:18:56 +0200

    chore: fix linting errors

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,13 @@ import antfu from "@antfu/eslint-config";
 
 export default await antfu({
   stylistic: false,
+  vue: {
+    // https://github.com/antfu/eslint-config/issues/367
+    sfcBlocks: {
+      blocks: {
+        styles: false,
+      },
+    },
+    vueVersion: 2,
+  },
 });
