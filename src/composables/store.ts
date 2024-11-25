11ce7f6b52af36be32ba8167ec6b539de4913611
Johann Schopplich <mail@johannschopplich.com>
2024-11-25 14:34:23 +0100

    refactor: error message

--- src/composables/store.ts
+++ src/composables/store.ts
@@ -1,17 +1,20 @@
 import { usePanel } from "./panel";
 
+type PanelAppStoreState = Record<string, any>;
+interface PanelAppStoreGetters {
+  "content/exists": (arg1?: any, arg2?: any) => any;
+  "content/hasChanges": (arg1?: any) => any;
+  "content/isCurrent": () => any;
+  "content/id": (arg1?: any) => any;
+  "content/model": (arg1?: any) => any;
+  "content/originals": (arg1?: any) => any;
+  "content/values": (arg1?: any) => any;
+  "content/changes": (arg1?: any) => any;
+}
+
 interface PanelAppStore {
-  state: Record<string, any>;
-  getters: {
-    "content/exists": (arg1?: any, arg2?: any) => any;
-    "content/hasChanges": (arg1?: any) => any;
-    "content/isCurrent": () => any;
-    "content/id": (arg1?: any) => any;
-    "content/model": (arg1?: any) => any;
-    "content/originals": (arg1?: any) => any;
-    "content/values": (arg1?: any) => any;
-    "content/changes": (arg1?: any) => any;
-  };
+  state: PanelAppStoreState;
+  getters: PanelAppStoreGetters;
 }
 
 /** @deprecated The Vuex store is removed in Kirby 5 */
@@ -21,7 +24,7 @@ export function useStore() {
 
   if (!store) {
     throw new Error(
-      "The Vuex store is not available. Are you using Kirby 5? Use the `useContent` composable instead.",
+      "Vuex store is not available. Are you using Kirby 5? Use the `useContent` composable instead.",
     );
   }
 
