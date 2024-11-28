bb19ebc14677870e02461256a2708732c707f429
Johann Schopplich <mail@johannschopplich.com>
2024-11-28 09:34:17 +0100

    feat: `isKirby4` and `isKirby5` constants

---
+++ src/composables/compatibility.ts
@@ -0,0 +1,2 @@
+export const isKirby4 = () => window.panel.plugins.viewButtons === undefined;
+export const isKirby5 = () => window.panel.plugins.viewButtons !== undefined;
--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,6 +1,7 @@
 export * from "./api";
 export * from "./app";
 export * from "./block";
+export * from "./compatibility";
 export * from "./content";
 export * from "./panel";
 export * from "./section";
