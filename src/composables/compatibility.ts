e734e162e7529632ef5cd62002b5272562cdf679
Johann Schopplich <mail@johannschopplich.com>
2024-12-09 11:32:22 +0100

    refactor: compat utils

--- src/composables/compatibility.ts
+++ src/composables/compatibility.ts
@@ -1,2 +1,2 @@
-export const isKirby4 = () => window.panel.plugins.viewButtons === undefined;
 export const isKirby5 = () => window.panel.plugins.viewButtons !== undefined;
+export const isKirby4 = () => !isKirby5();
