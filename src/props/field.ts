6216dc5c9e17aa6593d305e107b907d390ddacad
Johann Schopplich <johann@schopplich.com>
2026-09-03 11:04:27 +0200

    feat: add the field prop set

---
+++ src/props/field.ts
@@ -0,0 +1,27 @@
+import { disabled } from "./disabled";
+import { help } from "./help";
+import { id } from "./id";
+import { label } from "./label";
+import { name } from "./name";
+import { required } from "./required";
+
+/**
+ * @see https://github.com/getkirby/kirby/blob/main/panel/src/components/Forms/Field.vue
+ */
+export const field = {
+  ...disabled,
+  ...help,
+  ...id,
+  ...label,
+  ...name,
+  ...required,
+  counter: [Boolean, Object],
+  endpoints: Object,
+  hasDiff: Boolean,
+  input: {
+    type: [String, Number, Boolean],
+    default: null,
+  },
+  translate: Boolean,
+  type: String,
+};
