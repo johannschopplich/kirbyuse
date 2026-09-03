a74cbb9b6d2923c95130d12fb76690373245a52e
Johann Schopplich <johann@schopplich.com>
2026-09-03 11:04:27 +0200

    docs: import nothing but useBlock in the block example

--- src/composables/block.ts
+++ src/composables/block.ts
@@ -20,7 +20,7 @@ export interface BlockComponentProps {
  * @example
  * ```vue
  * <script setup>
- * import { computed, ref, useApi, usePanel, watch } from "kirbyuse";
+ * import { useBlock } from "kirbyuse";
  *
  * // Will inherit props from extended default block
  * const props = defineProps({});
